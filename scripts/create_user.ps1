673ea7d0894c15dbf072e3cf09add8aad9775505
Kevin Goncalo <kevingoncalo@gmail.com>
2025-12-23 18:46:26 -0500

    Add user creation script and entra user created screenshot

---
+++ scripts/create_user.ps1
@@ -0,0 +1,30 @@
+﻿# 1. Check if Microsoft.Graph module is installed - if not install
+
+if (-not (Get-Module -ListAvailable Microsoft.Graph)) {
+    Install-Module Microsoft.Graph -Scope CurrentUser -AllowClobber -Force
+}
+
+# 2. Connect to Microsoft Graph
+Connect-MgGraph -Scopes "User.ReadWrite.All"
+
+# 3. Define User Details
+$UserData = @{
+    DisplayName         = "Portfolio User"
+    UserPrincipalName   = "puser@kevshomelab.onmicrosoft.com"
+    MailNickname        = "puser"
+    UsageLocation       = "US"                     
+    AccountEnabled      = $true
+    PasswordProfile     = @{
+        Password = "TemporaryPass123!"
+        ForceChangePasswordNextSignIn = $true
+    }
+}
+
+# 4. Create the User
+try {
+    $NewUser = New-MgUser @UserData
+}
+catch {
+    Write-Error "Failed to create user. Error: $_"
+}
+
